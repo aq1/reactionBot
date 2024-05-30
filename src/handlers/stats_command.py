1a1747ded43d0958d6d18ea1437d80c6055bd1d5
aq1 <aq1@aq1.aq1>
2024-05-30 22:49:35 +0900

    Add stats command

--- src/handlers/stats_command.py
+++ src/handlers/stats_command.py
@@ -1,5 +1,20 @@
 from telegram import Update
 
+import db
+
 
 async def stats_command(update: Update, _):
-    await update.effective_message.reply_text('Coming soon')
+    stats = db.execute(
+        '''select telegram_user.username, telegram_user.full_name, emoji_id, count
+            from stat
+            left join telegram_user on telegram_user.id = stat.user_id
+            where chat_id = ?
+            order by count desc;''',
+        (update.effective_chat.id,)
+    )
+
+    text = []
+    for username, fullname, emoji_id, count in stats:
+        text.append(f'{username or fullname} {emoji_id} {count}')
+
+    await update.effective_chat.send_message('\n'.join(text))
