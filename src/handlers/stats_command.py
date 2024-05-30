220e7afd028b32233dec3c028fa70d67cc923d33
aq1 <aq1@aq1.aq1>
2024-05-30 23:37:56 +0900

    Changes

--- src/handlers/stats_command.py
+++ src/handlers/stats_command.py
@@ -1,20 +1,5 @@
 from telegram import Update
 
-import db
-
 
 async def stats_command(update: Update, _):
-    stats = db.execute(
-        '''select telegram_user.username, telegram_user.fullname, emoji_id, count
-            from stat
-            left join telegram_user on telegram_user.id = stat.user_id
-            where chat_id = ?
-            order by count desc;''',
-        (update.effective_chat.id,)
-    )
-
-    text = []
-    for username, fullname, emoji_id, count in stats:
-        text.append(f'{username or fullname} {emoji_id} {count}')
-
-    await update.effective_chat.send_message('\n'.join(text))
+    await update.effective_chat.send_message('WIP')
